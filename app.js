8c686dbd7fe24a54d418b44e22fe258ac81eb8c5
Abdi <letsbuildshhopify>
2022-04-28 01:10:42 +0100

    eventlistener functions + cart design

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const featuredImage = document.querySelector(".Feauture-img");
 const containerImage = document.querySelectorAll(".container-img");
 const prevNav = document.querySelector(".preNavigation");
 const nextNav = document.querySelector(".nextNavigation");
+const cart = document.querySelector(".cart");
+const plus = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const quantity = document.querySelector(".quantityNumber");
+const itemQuantity = document.querySelector(".quantityNumb");
+const finalPrice = document.querySelector(".final-price");
+const addToCart = document.querySelector(".add-to-cart");
+const cancel = document.querySelector(".delete-icon");
+const cartFull = document.querySelector(".cart-full");
 let count = 0;
+let cartCounter= 0;
 const imgArr = ["images/image-product-1.jpg", "images/image-product-2.jpg", "images/image-product-3.jpg", "images/image-product-4.jpg"];
 
 
@@ -24,13 +34,65 @@ prevNav.addEventListener("click", function(){
 // eventlistener to decrease counter which is used as an index for the imgArray to then retrive the src and populate the featured image
 nextNav.addEventListener("click", function(){
   count++
-  console.log(count)
   if(count >= 4){
     count = 0;
 }
 featuredImage.src = imgArr[count];
 })
 
+nextNav.addEventListener("click", function(){
+    count++
+    if(count >= 4){
+      count = 0;
+  }
+  featuredImage.src = imgArr[count];
+  })
+
+  function cartFunctionality (increase,decrease,cartBtn){
+    increase.addEventListener("click", function(){
+        cartCounter++
+        if(!cartCounter == 0){
+            cartFull.classList.add("flex");
+        }
+        quantity.textContent = cartCounter;
+        itemQuantity.textContent = cartCounter
+        let final = 125 * cartCounter;
+        finalPrice.textContent = `$ ${final}.00`
+        
+      })
+
+      decrease.addEventListener("click", function(){
+          if (cartCounter === 0){
+              let cartCounter = 0;
+          } else {
+            cartCounter--
+            if(cartCounter == 0){
+                cartFull.classList.remove("flex");
+            }
+            quantity.textContent = cartCounter;
+            itemQuantity.textContent = cartCounter
+            let final = 125 * cartCounter;
+            finalPrice.textContent = `$ ${final}.00`
+          }
+      })
+
+      cartBtn.addEventListener("click", function(){
+          cart.classList.add("block");
+      })
+  }
+
+ 
+      
+  
+
+  
+
+  cartFunctionality(plus,minus,addToCart);
+
+
+  
+
+  
 
 
 
